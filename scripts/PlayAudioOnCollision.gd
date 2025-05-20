5446ac3d546059eb855c0c08346c0a2bc2f4a161
PureDu4t <yumiracer@gmail.com>
2025-05-20 19:30:36 +0800

    test sniper

--- scripts/PlayAudioOnCollision.gd
+++ scripts/PlayAudioOnCollision.gd
@@ -7,7 +7,7 @@ func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _on_body_entered(body: Node) -> void:
-	if body.name == "Player":
+	if body.is_in_group("player"):
 		play_audio()
 
 func play_audio():
